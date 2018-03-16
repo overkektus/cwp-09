7013de49bac54444bf0fd4a370ae1b7eec5e4fbc
Egor Piskunou <egor.piskunou@gmail.com>
2018-03-16 23:22:33 +0300

    done task1.2

--- pokeapi.js
+++ pokeapi.js
@@ -9,3 +9,25 @@ axios.get('http://pokeapi.co/api/v2/pokemon/42')
   .catch(error => {
     console.error(error.message);
   });
+
+// Get data about 30 pokemon
+
+const promises = [];
+
+for(let i = 0; i < 3; i++) {
+  promises.push(axios.get(`http://pokeapi.co/api/v2/pokemon/?limit=10&offset=${i*10}`));
+}
+
+Promise.all(promises)
+  .then(res => {
+    for(let i = 0; i < promises.length; i++) {
+      console.log(`---------Promise[${i}]---------`);
+      res[i].data.results.forEach(element => {
+        console.log(element.name);
+      });
+    }
+  })
+  .catch(error => {
+    console.error('Error in fetch 30 pokemon' + error.message);
+  })
+

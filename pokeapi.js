865c715910a28985e4473ded6abf05ffdec1decb
Egor Piskunou2 <egor.piskunou@gmail.com>
2018-03-21 22:52:39 +0300

    done task1

--- pokeapi.js
+++ pokeapi.js
@@ -1,3 +1,4 @@
+const Promise = require('bluebird');
 const axios = require('axios');
 
 // Get data about pokemon with id 42
@@ -31,3 +32,58 @@ Promise.all(promises)
     console.error('Error in fetch 30 pokemon' + error.message);
   })
 
+// pokemon with id = [1, 4, 7]
+
+Promise.any([
+  axios.get('http://pokeapi.co/api/v2/pokemon/1'),
+  axios.get('http://pokeapi.co/api/v2/pokemon/4'),
+  axios.get('http://pokeapi.co/api/v2/pokemon/7')
+])
+  .then((result) => {
+    console.log(`first is: ${result.data.name}`);
+  })
+  .catch((err) => {
+    console.log('error in any')
+    console.error(err);
+  })
+
+  //10 pokemons, items and locations
+
+Promise.props({
+    pokemons: axios.get(`https://pokeapi.co/api/v2/pokemon/?limit=10`),
+    items: axios.get(`https://pokeapi.co/api/v2/item/?limit=10`),
+    locations: axios.get(`https://pokeapi.co/api/v2/location/?limit=10`)
+  })
+  .then((result) => {
+    console.log("POKEMONS");
+    result["pokemons"].data.results.forEach((val) => {
+      console.log(val.name);
+    });
+    console.log("ITEMS");
+    result["items"].data.results.forEach((val) => {
+      console.log(val.name);
+    });
+    console.log("LOCATIONS");
+    result["locations"].data.results.forEach((val) => {
+      console.log(val.name);
+    });
+  })
+  .catch((err) => {
+    console.log('error in pokemons/items/locations')
+    console.error(err);
+})
+
+//get 4 berries
+
+Promise.map([1, 2, 3, 4], (i) => {
+    return axios.get(`http://pokeapi.co/api/v2/pokemon/${i}`)
+  })
+  .then((result) => {
+    result.forEach((val) => {
+      console.log(val.data.name);
+    })
+  })
+  .catch((err) => {
+    console.log('error in berries');
+    console.error(err);
+  })
